ed9528f365ec2ef1d0f2681a52e80e724e696a69
Марков Илья Романович <ilya.markov212001@gmail.com>
2022-04-11 13:49:42 +0300

    second_commit

--- IS_1/main.py
+++ IS_1/main.py
@@ -1,9 +1,12 @@
 #https://github.com/MarkovIlya/Task_for_Infomation_Security_1
 import cryptocode
 from passlib.hash import md5_crypt
-pas = md5_crypt.hash("password")
+pas = input()
+password = input()
+pas = md5_crypt.hash(pas)
 print(pas)
-login_encoded = cryptocode.encrypt("B0$$_of_ADm1nS", pas)
+login_encoded = cryptocode.encrypt(password, pas)
+print(login_encoded)
 ## And then to decode it:
 login_decoded = cryptocode.decrypt(login_encoded, pas)
 print(login_decoded)
