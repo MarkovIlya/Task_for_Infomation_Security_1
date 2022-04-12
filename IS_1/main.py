a861df58f7b815e3bc0f40ad6361bc2a1c1020f5
Марков Илья Романович <ilya.markov212001@gmail.com>
2022-04-12 09:48:24 +0300

    vadim_commit

--- IS_1/main.py
+++ IS_1/main.py
@@ -1,6 +1,7 @@
-#https://github.com/MarkovIlya/Task_for_Infomation_Security_1
 import cryptocode
 from passlib.hash import md5_crypt
+from vadim import Point
+
 pas = input()
 password = input()
 pas = md5_crypt.hash(pas)
@@ -10,3 +11,5 @@ print(login_encoded)
 ## And then to decode it:
 login_decoded = cryptocode.decrypt(login_encoded, pas)
 print(login_decoded)
+
+
---
+++ IS_1/vadim.py
@@ -0,0 +1,3 @@
+class Point:
+    'https://github.com/MarkovIlya/Task_for_Infomation_Security_1'
+    pass
